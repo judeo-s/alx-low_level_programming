a9970f16db9272c44460fb3e2d3c6b309eab45a9
Jude OS <jos@example.com>
2023-10-06 23:29:15 +0100

    Wrote a program that prints all single digit numbers of base 10 starting from 0 without using printf, followed by a new line.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 <= 9; base10++)
+	{
+		putchar(48 + base10);
+	}
+	putchar('\n');
+	return (0);
+}
