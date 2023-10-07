e86675e7d250c10bff92b3869ce4ab5c7ac5d665
Jude OS <jos@example.com>
2023-10-07 05:19:23 +0100

    Wrote a program that prints all the numbers of base 16 in lowercase, followed by a new line

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry Point
  *
- * Description: prints all single digit numbers of base 10
+ * Description: prints all single digit numbers of base 16
  *
  * Return: Always 0 (Success)
 */
@@ -10,10 +10,16 @@
 int main(void)
 {
 	int base10;
+	char alpha;
 
 	for (base10 = 0; base10 <= 9; base10++)
 	{
 		putchar(48 + base10);
+	}
+
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
 	}
 
 	putchar('\n');
