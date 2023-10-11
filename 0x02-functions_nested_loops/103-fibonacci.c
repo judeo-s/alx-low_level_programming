5dfaa5a049d0f8c2775a2f5fd436e64b0e61ab01
Jude OS <jos@example.com>
2023-10-11 23:22:29 +0100

    Just finished with task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Startting point for the code
+ *
+ * Description: Prints the first 50 Fibonacci numbers
+ * Return: Returns 0 if successful
+ */
+int main(void)
+{
+	unsigned long t1 = 1, t2 = 2, nextTerm = 0, sum = 2;
+
+	while (nextTerm <= 4000000)
+	{
+
+		nextTerm = t1 + t2;
+		
+		if (nextTerm % 2 ==0)
+		{
+			sum += nextTerm;
+		}
+
+		t1 = t2;
+		t2 = nextTerm;
+	}
+
+	printf("%lu\n", sum);
+	return (0);
+}
+
