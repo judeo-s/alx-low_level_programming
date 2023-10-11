016632f679a73aa51e72d7c05a6c90419ae7e9e5
Jude OS <jos@example.com>
2023-10-11 23:28:21 +0100

    Finished with task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Startting point for the code
+ *
+ * Description: Prints the first 50 Fibonacci numbers
+ * Return: Returns 0 if successful
+ */
+int main(void)
+{
+	unsigned long t1 = 1, t2 = 2, nextTerm = 0;
+	int i;
+
+	printf("%lu, %lu, ", t1, t2);
+
+	for (i = 3; i <= 98; ++i)
+	{
+
+		nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
+
+		if (i != 98)
+		{
+			printf("%lu, ", nextTerm);
+		}
+		else
+		{
+			printf("%lu\n", nextTerm);
+		}
+	}
+	return (0);
+}
+
