1db39de3ac8150698fd377ec7322ee4aac5f6b7d
Jude OS <jos@example.com>
2023-10-12 20:13:06 +0100

    Completed task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks if letter is number or not
+ *
+ * @c: Integer input
+ * Return: Integer(1) if successful and (0) if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
