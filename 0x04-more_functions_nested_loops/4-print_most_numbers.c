d55e2aa50006abc922d7dd824dd26f987cd1aa0b
Jude OS <jos@example.com>
2023-10-12 21:30:50 +0100

    Done with task 5

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0-9 to the screen
+ * _putchar - Print character to screen
+ *
+ * Return: None
+ */
+void print_most_numbers(void)
+{
+	char digit;
+
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		if (digit != '2' || digit != '4')
+		{
+			_putchar(digit);
+		}
+	}
+
+	_putchar('\n');
+}
