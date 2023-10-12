459445f5a62e1230ad50aa54acc2c0d645a85ea9
Jude OS <jos@example.com>
2023-10-12 20:17:45 +0100

    Done with task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - Multiplies two integers and returns the result
+ *
+ * @x: Integer input
+ * @y: Integer input
+ * Return: Int, the result of the addition.
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
