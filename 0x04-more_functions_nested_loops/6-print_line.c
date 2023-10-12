1f7bceaf27d470ee0b9388bd05450222456a7e21
Jude OS <jos@example.com>
2023-10-12 22:39:35 +0100

    Had a bug in task 7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,15 +12,15 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (lines = 0; lines < n; lines++)
+		for (lines = 1; lines <= n; ++lines)
 		{
 			_putchar('_');
 		}
+
+		_putchar('\n');
 	}
 	else
 	{
 		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
