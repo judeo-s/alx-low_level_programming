57250e9c92315864b4cd72c68f4237c8b6a651ac
Jude OS <jos@example.com>
2023-10-13 00:14:24 +0100

    Finished with task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - This starts the program and prints
+ *	Fizz for multiples of 5
+ *	Buzz for multiples of 3
+ *	FizzBuzz for multiples of 3 and 5
+ *
+ * Return: int
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; ++num)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0 && !(num % 3 == 0))
+		{
+			printf("Buzz");
+		}
+		else if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
+
+		if (num != 100)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+
+	return (0);
+}
