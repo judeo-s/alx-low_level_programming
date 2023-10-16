9f7f6083d96313ef5f2b967f90ca3794a2d7c7b8
Jude OS <jos@example.com>
2023-10-16 21:43:31 +0100

    Done with task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the size of a string
+ *
+ * @s: Pointer to an char variable
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}
+
+
+/**
+ * print_rev - a function that prints a string in reverse
+ *
+ * @str: Pointer to an char variable
+ * Return: void
+ */
+void print_rev(char *str)
+{
+	int i;
+
+	for (i = _strlen(str); i >= 0; --i)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
