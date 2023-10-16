26bb5f936b48927cd5c75e51ae2a19d2051cfb09
Jude OS <jos@example.com>
2023-10-16 21:26:36 +0100

    Finished with task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ *
+ * @str: Pointer to an char variable
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
