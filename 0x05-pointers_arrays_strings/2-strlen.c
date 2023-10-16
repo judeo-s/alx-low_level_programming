b6b12e2fc03cc9398027dd7fd4ef71430c9dd2cb
Jude OS <jos@example.com>
2023-10-16 21:09:54 +0100

    Done with task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the size of a string
+ *
+ * @s: Pointer to an char variable
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		i++;
+	}
+
+	return (i);
+}
