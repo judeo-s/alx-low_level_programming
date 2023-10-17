84a18ec92fd7b39d323ee3b409b966af72d32e45
Jude OS <jos@example.com>
2023-10-17 06:44:42 +0100

    Done with task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s: string input parameter
+ *
+ * Return: converted integer from string
+*/
+
+int _atoi(char *s)
+{
+	unsigned int number = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			number = (number * 10) + (*s - '0');
+		}
+		else if (number > 0)
+		{
+			break;
+		}
+	} while (*s++);
+
+	return (number * sign);
+}
