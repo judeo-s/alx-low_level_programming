60e2103f476e6426baf197be27761af3a6bfb001
Jude OS <jos@example.com>
2023-10-19 14:30:39 +0100

    Had to fix a few issues with task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -30,26 +30,18 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len = _strlen(dest);
 	int src_len = _strlen(src);
-
-	static char temp[1000];
-	int counter = 0;
+	int counter = dest_len;
 	int i;
-
-	for (i = 0; i < dest_len; i++)
-	{
-		temp[counter] = dest[i];
-		counter++;
-	}
 
 	if (n < src_len)
 		src_len = n;
 
 	for (i = 0; i < src_len; i++)
 	{
-		temp[counter] = src[i];
 		dest[counter] = src[i];
 		counter++;
 	}
-	temp[counter] = '\0';
-	return (temp);
+
+	dest[counter] = '\0';
+	return (dest);
 }
