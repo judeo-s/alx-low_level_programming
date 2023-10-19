d87af2a1be192e6f0f924c8cd6f8289884cb9604
Jude OS <jos@example.com>
2023-10-19 18:02:43 +0100

    Had to tweek a few things on task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -21,20 +21,19 @@ int _strlen(char *s)
 
 /**
  * string_toupper - a function to convert all lowercase characters
- * 		in a string to uppercase
+ *                  in a string to uppercase
  *
  * @a: char pointer
- * 
  * Return: char pointer
  */
 char *string_toupper(char *a)
 {
 	int str_len, i;
-	str_len = _strlen(a);
 
+	str_len = _strlen(a);
 	for (i = 0; i < str_len; i++)
 	{
-		if(a[i] >= 'a' && a[i] <= 'z')
+		if (a[i] >= 'a' && a[i] <= 'z')
 		{
 			a[i] = a[i] - 32;
 		}
