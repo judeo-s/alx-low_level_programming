f0e72b48345dc21a10c032b26d75d66a4afad72e
Jude OS <jos@example.com>
2023-10-19 15:50:27 +0100

    Done with solving task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the size of a string
+ *
+ * @s: Pointer to an char variable
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}
+
+/**
+ *_strcmp - a function to copies a strings to n-size of another string
+ *
+ * @s1: char pointer
+ * @s2: char pointer
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int src_len = _strlen(s1);
+	int dest_len = _strlen(s2);
+	int i, result = 0;
+
+	for (i = 0; i < src_len; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			continue;
+		}
+		else
+		{
+			if (src_len > dest_len)
+			{
+				result = 15;
+				break;
+			}
+			else
+			{
+				result = -15;
+				break;
+			}
+		}
+	}
+
+	return (result);
+}
