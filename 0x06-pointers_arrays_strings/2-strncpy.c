a68528d1a52e2d0696a9e79181abcbfde751760a
Jude OS <jos@example.com>
2023-10-19 15:01:19 +0100

    Done with solving task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the size of a string
+ *
+ * @s: Pointer to an char variable
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}
+
+/**
+ *_strncpy - a function to copies a strings to n-size of another string
+ *
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: int
+ * Return: char pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int src_len = _strlen(src);
+	int i;
+
+	if (n < src_len)
+	{
+
+		for (i = 0; i < n; i++)
+		{
+			dest[i] = src[i];
+		}
+		dest[i] = '\0';
+	}
+	else
+	{
+		for (i = 0; i < n && src[i] != '\0'; i++)
+		{
+			dest[i] = src[i];
+		}
+		for (; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+	}
+	return (dest);
+}
