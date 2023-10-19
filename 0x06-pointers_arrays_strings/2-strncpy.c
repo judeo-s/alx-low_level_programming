8363929e893f1f9b02398d194555a63d5469d45e
Jude OS <jos@example.com>
2023-10-19 15:03:30 +0100

    Had to do few changes on task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -28,28 +28,16 @@ int _strlen(char *s)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int src_len = _strlen(src);
 	int i;
 
-	if (n < src_len)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-
-		for (i = 0; i < n; i++)
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
 		dest[i] = '\0';
 	}
-	else
-	{
-		for (i = 0; i < n && src[i] != '\0'; i++)
-		{
-			dest[i] = src[i];
-		}
-		for (; i < n; i++)
-		{
-			dest[i] = '\0';
-		}
-	}
+	
 	return (dest);
 }
