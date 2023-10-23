633327aebf4d4abe8adb988f904ed32d58b2c0ef
Jude OS <jos@example.com>
2023-10-23 21:00:33 +0100

    Finished with task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the size of a string
+ *
+ * @s: Pointer to an char variable
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}
+
+
+/**
+ *_strstr - a function that locates a substring.
+ *
+ * @haystack: char *
+ * @needle: char *
+ * Return: char *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int needle_len = _strlen(needle);
+	int y, flag;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			flag = 0;
+			for (y = 0; y < needle_len; y++)
+			{
+				if (haystack[y] == needle[y])
+				{
+					flag = 1;
+				}
+			}
+			if (flag)
+			{
+				return (haystack);
+			}
+		}
+		haystack++;
+	}
+	return (NULL);
+}
