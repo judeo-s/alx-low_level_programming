28fcc80e9abfaceb69de4456ace8d8b55d01f8eb
Jude OS <jos@example.com>
2023-10-27 17:17:48 +0100

    Ran the bash script through betty

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c 1* 0* 2* 3* 4* 5* 6* 9* _putchar.c
+gcc -Wall -Werror -Wextra -pedantic\
+ -std=gnu89 -c 1* 0* 2* 3* 4* 5* 6* 9* _putchar.c
 ar rc liball.a *.o
