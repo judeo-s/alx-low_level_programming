e3a9a91cc06a7f40336eeedbc702df1fce274b83
Jude OS <jos@example.com>
2023-10-31 13:57:57 +0000

    Done with task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * init_0 - a function that sets all the values
+ *          in a multi-dimensional array to zeros.
+ *
+ * @array: int **
+ * @height: int
+ * @width: int
+ * Return: void
+ */
+void init_0(int **array, int height, int width)
+{
+	int x, y;
+
+	for (x = 0; x < height; x++)
+	{
+		for (y = 0; y < width; y++)
+		{
+			array[x][y] = 0;
+		}
+	}
+}
+
+
+/**
+ * alloc_grid - a function that returns a pointer to a 2 dimensional
+ *              array of integers.
+ *
+ * @width: int
+ * @height: int
+ * Return: int **
+ */
+int **alloc_grid(int width, int height)
+{
+	int **multi_array;
+	int x;
+
+	if (width <= 0 || height <= 0)
+	{
+		multi_array = NULL;
+	}
+	else
+	{
+		multi_array = (int **)malloc(sizeof(int) * height);
+		for (x = 0; x < height; x++)
+		{
+			multi_array[x] = (int *)malloc(sizeof(int) * width);
+		}
+		init_0(multi_array, height, width);
+	}
+	return (multi_array);
+}
+
+
+
