704485901a3c22dd729db9012808c707515109e0
Jude OS <jos@example.com>
2023-10-31 09:38:37 +0000

    Fixed some bugs in task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ * _strlen - a function that returns the size of a string
+ *
+ * @s: Pointer to an char variable
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}
+
+
+
+/**
  * _strdup - a function that returns a pointer to a newly
  *           allocated space in memory, which contains a copy
  *           of the string given as a parameter.
@@ -11,7 +31,7 @@
 char *_strdup(char *str)
 {
 	static char *string;
-	int size = sizeof(str);
+	int size = _strlen(str);
 
 	if (str == NULL)
 	{
@@ -23,11 +43,14 @@ char *_strdup(char *str)
 
 		string = (char *)malloc(sizeof(char) * size);
 
-		for (x = 0; x < size; x++)
+		if (string == NULL)
+		{
+			return (string);
+		}
+		for (x = 0; x <= size; x++)
 		{
 			string[x] = str[x];
 		}
-		string[x] = '\0';
 	}
 	return (string);
 }
