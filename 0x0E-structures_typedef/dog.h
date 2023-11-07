d822c0da9d1c2e98dca614c9c364bdd5869d9f76
Jude OS <jos@example.com>
2023-11-07 01:35:27 +0000

    forgot to add to the header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_HEADER
 
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * struct dog - a struct used to collect data about the owner of a dog
  * @name: char *
@@ -22,5 +22,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
