39fbd9768e90dd481b8393006666833551a09389
Jude OS <jos@example.com>
2023-11-09 19:21:25 +0000

    Fixed a few issues with betty on task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,25 +14,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *temp;
 
-	if (n > 0)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL)
-		{
-			va_start(args, n);
-			for (i = 0; i < n; i++)
-			{
-				temp = va_arg(args, char *);
+		temp = va_arg(args, char *);
 
-				if (temp != NULL)
-					printf("%s", temp);
-				else
-					printf("(nil)");
+		if (temp != NULL)
+			printf("%s", temp);
+		else
+			printf("(nil)");
 
-				if (i != (n - 1))
-					printf("%s", separator);
-			}
+		if (i != (n - 1) && separator)
+			printf("%s", separator);
+	}
 			va_end(args);
 			printf("\n");
-		}
-	}
 }
