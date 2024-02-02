cf917228dce05e954e53181ff00baefe47fc37a0
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-02-02 13:57:23 +0000

    Done with task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - a function that returns the index of a key
+ *
+ * @key: const unsigned char *
+ * @size: unsigned long int
+ * Return: unsigned long int
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);	
+}
