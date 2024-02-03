012b59ea5568db299b0c15838a3fa8559c7f88b8
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-02-03 14:39:13 +0000

    Fixed some spacing issues

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,9 +20,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	temp = ht->array[index];
 
-	while(temp)
+	while (temp)
 	{
-		if(!strcmp(temp->key, key))
+		if (!strcmp(temp->key, key))
 			return (temp->value);
 		temp = temp->next;
 	}
